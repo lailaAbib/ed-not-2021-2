8e801186cea48453d2b887cc3329d3c5120d0a16
lailaAbib <laylaabib45@gmail.com>
2021-08-06 23:24:32 +0000

    (06/08) Funções e início da busca sequencial

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,5 @@
+
+from math import pi
 # Função é um preço de código que tem um nome e pode receber informações
 # externas para fazer o seu trabalho. Opcionalmente, uma função pode também
 # produzir um valor de resultado.
@@ -19,4 +21,26 @@ a = float(input('Informe a altura da pessoa: '))
 # Fazendo uma chamada de função imc()
 resultado = imc(p, a)
 
+
 print(f"O IMC calculado é {resultado}.")
+
+print('------------------------------------')
+
+def area_forma(base, altura, forma = "R"):
+    area = 0
+    if forma == "R": # Retângulo
+        area = base * altura
+    elif forma == "T":  # Triângulo
+        area = base * altura / 2
+    elif forma == "E": # Elipse
+        area = (base / 2) * (altura / 2) * pi
+    return area
+
+print('------------------------------------')
+
+print(f"Retângulo 7.5x11: {area_forma(7.5, 11, 'R')}")
+print(f"Triângulo 78x12: {area_forma(8, 12, 'T')}")
+print(f"Circulo 15x15: {area_forma(15, 15, 'E')}")
+print(f"Quadrado 9.5x9.5: {area_forma(9.5, 9.5)}")
+
+
