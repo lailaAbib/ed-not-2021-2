ca853b3b6f75fdb3d464f116c424ec7b7e6d706b
lailaAbib <laylaabib45@gmail.com>
2021-09-02 23:24:22 +0000

    (02/09) merge e quick

--- 05.recursividade.py
+++ 05.recursividade.py
@@ -1,14 +1,25 @@
-# 3! = 3 . 2 . 1 = 6
-# 4! = 4. 3 . 2 . 1 = 24
-# 5! = 5 . 4 . 3 . 2 . 1 = 120
-# 6! = 6 . 5 . 4 . 3 . 2 . 1 = 720
-# 7! = 7 . 6 . 5 . 4 . 3 . 2 . 1 = 5.040
-# 8! = 8 . 7 . 6 . 5 . 4 . 3 . 2 . 1 = 40.320
-# 9! = 9 . 8 . 7 . 6 . 5 . 4 . 3 . 2 . 1 = 362.880
-# 10! = 10 . 9 . 8 . 7 . 6 . 5 . 4 . 3 . 2 . 1 = 3.628.800
+# FATORIAL DE UM NÚMERO n
+# É igual ao número n multiplicado por todos os seus
+# antecessores até 1
+#
+# 7! = 7 * 6 * 5 * 4 * 3 * 2 * 1 (5040)
+#
+# 5! = 5 * 4 * 3 * 2 * 1 (120)
+# 4! = 4 * 3 * 2 * 1 (24)
+# 3! = 3 * 2 * 1 (6)
+# 2! = 2 * 1 (2)
+# 1! = 1
+# 0! = 1 (por convenção)
+#
+# 5! = 5 * 4!
+# 4! = 4 * 3!
+# 3! = 3 * 2!
+# 2! = 2 * 1!
+# 1! = 1 * 0!
 
+# n! = n * (n - 1)! p/ n > 1
 
-# Implementação interativo
+# Implementação iterativa (iter = caminho)
 def fatorial(n):
     resultado = 1
     if(n > 1):
@@ -22,21 +33,23 @@ print(f'5! = {fatorial(5)}')
 print(f'7! = {fatorial(7)}')
 
 # n! = n * (n - 1)! p/ n > 1
-# Recursividade ocorre quando definição de uma função inclui a própia
+# Recursividade ocorre quando a definição de uma função inclui a própria
 # função sendo definida
-
 
 # Em programação, a recursividade se traduz quando uma função efetua
 # chamadas a si própria.
 
 # Implementação recursiva
 def fatorial2(n):
-    # Em uma função recursiva, a condição de saída é aquela em que 
+    # Em uma função recursiva, a condição de saída é aquela em que
     # a função é capaz de retornar um resultado SEM chamar novamente
     # a si mesma
-    if n <= 1: # Condição de saída
+    if n <= 1:  # Condição de saída
         return 1
     return n * fatorial2(n - 1)
 
-print('-------------------------------------')
-print(f'519! = {fatorial(519)}')
+print('---------------------------------------------------')
+print(f'519! = {fatorial2(519)}')
+
+#import sys
+#print(sys.maxsize)
