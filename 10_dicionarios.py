c4dd8344f6ed4254d2111cfc0b8dbecbe962f140
lailaAbib <laylaabib45@gmail.com>
2021-09-09 23:31:00 +0000

    (09/09) Introdução a POO

--- 10_dicionarios.py
+++ 10_dicionarios.py
@@ -1,22 +1,42 @@
+# Dicionário é uma estrutura da linguagem Python
+# capaz de armazenar múltiplos valores em uma única
+# variável, por meio de pares de chave-valor
+pessoa = {
+    # "nome" é a chave
+    # "Fulano de Tal" é o valor
+    "nome": "Fulano de Tal",
+    "sexo": "M",
+    "idade": 39,
+    "peso": 76,
+    "altura": 1.82
+}
+
+# Calculando o IMC (Índice de Massa Corporal)
+imc = pessoa["peso"] / (pessoa["altura"] ** 2)
+print(f"O IMC de {pessoa['nome']} é {imc}.")
+
 forma1 = {
-    "base": ,
-    "altura": ,
-    "tipo":
+    "base": 7.5,
+    "altura": 12,
+    "tipo": "R"     # Retângulo
 }
+
 forma2 = {
-    "base": ,
-    "altura": ,
-    "tipo":
+    "base": 6,
+    "altura": 2.5,
+    "tipo": "T"     # Triângulo
 }
+
 forma3 = {
-    "base": ,
-    "altura": ,
-    "tipo":
+    "base": 5,
+    "altura": 3,
+    "tipo": "E"     # Elipse
 }
+
 forma4 = {
     "base": 10,
     "altura": 5,
-    "tipo": "W" # tipo não reconhecido
+    "tipo": "W"     # Tipo não reconhecido
 }
 
 forma5 = {
@@ -24,21 +44,22 @@ forma5 = {
     "fruta": "abacate",
     "tipo": "T"
 }
+
 from math import pi
 
 def calcular_area(forma):
-    if forma["tipo"] == "R": #Retâgulo
+    if forma["tipo"] == "R":    # Retângulo
         return forma["base"] * forma["altura"]
-    elif forma["tipo"] == "T": #Triângulo
-        return forma["base"] * forma["altura / 2"]
-    elif forma["tipo"] == "E": # Elipse
+    elif forma["tipo"] == "T":  # Triângulo
+        return forma["base"] * forma["altura"] / 2
+    elif forma["tipo"] == "E":  # Elipse
         return forma["base"] / 2 * forma["altura"] / 2 * pi
     else:
         # Gera um erro
-        raise Exception("Tipo de forma não reconhecido.")
+        raise Exception("Tipo de forma não reconhecido.") 
 
-print(f"Área de um Retângulo de 7.5x12: {calcular_area(forma1)}")
-print(f"Área de um Triângulo de 6x2.5: {calcular_area(forma2)}")
-print(f"Área de uma Elipse de 5x3: {calcular_area(forma3)}")
-#print(f"Área de uma forma Desconhecida de 10x5: {calcular_area(forma4)}")
-print(f"Área de uma forma Desconhecida de ?x?: {calcular_area(forma5)}")
+print(f"Área de um retângulo de 7.5x12: {calcular_area(forma1)}")
+print(f"Área de um triângulo de 6x2.5: {calcular_area(forma2)}")
+print(f"Área de uma elipse de 5x3: {calcular_area(forma3)}")
+# print(f"Área de uma forma desconhecida de 10x5: {calcular_area(forma4)}")
+print(f"Área de uma forma desconhecida de ?x?: {calcular_area(forma5)}")
--- 11_classes.py
+++ 11_classes.py
@@ -19,9 +19,16 @@ class FormaGeometrica():
 
     # Este método é executado quando um objeto é criado a partir
     # de uma classe
-    def __init__(self, base = 0, altura = 0, tipo = "R"):
+    def __init__(self, base, altura, tipo = "R"):
         # Recebe os valores passados ao construtor e os armazena
         # dentro dos atributos
+        if base <= 0:
+            raise Exception("A base deve ser maior que zero.")
+        elif altura <= 0:
+            raise Exception("A altura deve ser maior que zero.")
+        elif tipo not in ["R", "T", "E"]:
+            raise Exception("O tipo deve ser R, T ou E.")
+
         self.base = base
         self.altura = altura
         self.tipo = tipo
@@ -30,6 +37,9 @@ class FormaGeometrica():
 
 # Criando objetos a partir da classe
 
-retangulo1 = FormaGeometrica(15, 10, "R")
+retangulo1 = FormaGeometrica(15, 10, "R") # Chama o __init__
+triangulo1 = FormaGeometrica(6, 9, "T")
 
 print(f"[retangulo1] base: {retangulo1.base}, altura: {retangulo1.altura}, tipo: {retangulo1.tipo}")
+
+print(f"[triangulo1] base: {triangulo1.base}, altura: {triangulo1.altura}, tipo: {triangulo1.tipo}")
